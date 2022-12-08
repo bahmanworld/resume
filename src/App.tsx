fa63d993155b56d34aedf183560793824b951936
bahman <bahman.world@gmail.com>
2022-12-09 01:49:20 +0330

    Creating new UI 🪸

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import Avatar from './assets/avatar.png'
-import './App.css'
+import { useState } from "react";
+import Avatar from "./assets/avatar.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <img src={Avatar} style={{width: 200, height: 200, borderRadius: 250}} />
-      <h1>Bahman World 2022</h1>
-      <br/>
-      <br/>
-      <a href='mailto:bahman.world@gmail.com'>Mail Me Now</a>
-      <br/>
-      <br/>
-      <a href='tel:+989367036819'>Call Me</a>
+    <div id="app">
+      <div id="box">
+        <header>
+          
+        </header>
+        <div id="inner">Bahman World</div>
+      </div>
     </div>
-  )
+  );
 }
-export default App
+export default App;
